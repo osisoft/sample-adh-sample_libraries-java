c156cd78201943d824f74e9b1cc26deee6f40dea
Kelsey Duffy <kduffy@osisoft.com>
2021-05-21 08:49:03 -0400

    daily checkpoint may 20

--- src/main/java/com/github/osisoft/ocs_sample_library_preview/dataviews/Field.java
+++ src/main/java/com/github/osisoft/ocs_sample_library_preview/dataviews/Field.java
@@ -1,14 +1,49 @@
 package com.github.osisoft.ocs_sample_library_preview.dataviews;
 
+import com.github.osisoft.ocs_sample_library_preview.sds.SdsSummaryType;
+
 public class Field {
     private FieldSource Source;
     private String[] Keys;
     private String Label;
+    private boolean IncludeUom;
+    private SummaryDirection SummaryDirection;
+    private SdsSummaryType SdsSummaryType;
 
     public Field(FieldSource source, String[] keys, String label) {
         this.Source = source;
         this.Keys = keys;
         this.Label = label;
+        this.IncludeUom = false;
+        this.SummaryDirection = com.github.osisoft.ocs_sample_library_preview.dataviews.SummaryDirection.Forward;
+        this.SdsSummaryType = com.github.osisoft.ocs_sample_library_preview.sds.SdsSummaryType.None;
+    }
+
+    public Field(FieldSource source, String[] keys, String label, boolean includeUom) {
+        this.Source = source;
+        this.Keys = keys;
+        this.Label = label;
+        this.IncludeUom = includeUom;
+        this.SummaryDirection = com.github.osisoft.ocs_sample_library_preview.dataviews.SummaryDirection.Forward;
+        this.SdsSummaryType = com.github.osisoft.ocs_sample_library_preview.sds.SdsSummaryType.None;
+    }
+
+    public Field(FieldSource source, String[] keys, String label, boolean includeUom, SummaryDirection summaryDirection, SdsSummaryType sdsSummaryType) {
+        this.Source = source;
+        this.Keys = keys;
+        this.Label = label;
+        this.IncludeUom = includeUom;
+        this.SummaryDirection = summaryDirection;
+        this.SdsSummaryType = sdsSummaryType;
+    }
+
+    public Field(Field field) {
+        this.Source = field.Source;
+        this.Keys = field.Keys;
+        this.Label = field.Label;
+        this.IncludeUom = field.IncludeUom;
+        this.SummaryDirection = field.SummaryDirection;
+        this.SdsSummaryType = field.SdsSummaryType;
     }
 
     public FieldSource getSource() {
@@ -34,4 +69,18 @@ public class Field {
     public void setLabel(String label) {
         this.Label = label;
     }
+
+    public boolean getIncludeUom() {
+        return IncludeUom;
+    }
+
+    public void setIncludeUom(boolean includeUom) { this.IncludeUom = includeUom; }
+
+    public SummaryDirection getSummaryDirection() { return SummaryDirection; }
+
+    public void setSummaryDirection(SummaryDirection summaryDirection) { this.SummaryDirection = summaryDirection; }
+
+    public SdsSummaryType getSdsSummaryType() { return SdsSummaryType; }
+
+    public void setSdsSummaryType(SdsSummaryType sdsSummaryType) { this.SdsSummaryType = sdsSummaryType; }
 }
---
+++ src/main/java/com/github/osisoft/ocs_sample_library_preview/sds/SdsSummaryType.java
@@ -0,0 +1,22 @@
+package com.github.osisoft.ocs_sample_library_preview.sds;
+
+public enum SdsSummaryType {
+
+    None("None"), Count("Count"), Minimum("Minimum"),
+    Maximum("Maximum"), Range("Range"), Mean("Mean"),
+    StandardDeviation("StandardDeviation"),
+    PopulationStandardDeviation("PopulationStandardDeviation"),
+    Total("Total"), Skewness("Skewness"), Kurtosis("Kurtosis"),
+    WeightedMean("WeightedMean"), WeightedStandardDeviation("WeightedStandardDeviation"),
+    WeightedPopulationStandardDeviation("WeightedPopulationStandardDeviation");
+
+    private final String SdsSummaryType;
+
+    private SdsSummaryType(String id) {
+        this.SdsSummaryType = id;
+    }
+
+    public String getValue() {
+        return SdsSummaryType;
+    }
+}
